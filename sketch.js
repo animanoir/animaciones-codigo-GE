eafed19e1ed69182ca5295d656e8d11e0c738938
Oscar <etherealflux94@gmail.com>
2016-09-04 21:29:53 -0500

    todos los assets puestos en orden

--- sketch.js
+++ sketch.js
@@ -1,10 +1,24 @@
+function preload(){
+    caracol = loadImage("media/animacion1/caracol.png");
+    hormiga = loadImage("media/animacion1/hormiga.png");
+    cronometro = loadImage("media/animacion1/cronometro.png");
+    fondo = loadImage("media/animacion1/fondo.png");
+}
+
 function setup() {
-    var cnv = createCanvas(800, 600);
+    var cnv = createCanvas(950, 400);
     cnv.parent("p5");
 
-    background(0);
+    imageMode(CENTER);
 }
 
 function draw() {
+    console.log(mouseX, mouseY);
+
+    background(255);
+
+    image(fondo, 392, 216);
+    image(hormiga, 116, 133);
+    image(caracol, 116, 313);
 
 }
